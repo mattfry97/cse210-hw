c4b64b5ee17f34a89ea87412a58ec9f92147c4b5
mattfry97 <fry21002@byui.edu>
2025-01-11 23:50:28 -0700

    finished the assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,12 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    static void Main(string[] args){
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
