1a79d1d7616c54ebdc96bae1f7380cc60b850eec
mattfry97 <fry21002@byui.edu>
2025-01-16 22:11:10 -0700

    finished the assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program{
+    static void Main(string[] args){
+        Random randomGenerator = new Random();
+        int randomNum = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != randomNum){
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (randomNum > guess){
+                Console.WriteLine("Higher");
+            }
+            else if (randomNum < guess){
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
