66e062600310367f10301ec4bf09475dfdce4979
mattfry97 <fry21002@byui.edu>
2025-03-01 16:31:49 -0700

    final project done

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,26 @@
+public class BreathingActivity : Activity{
+    public BreathingActivity() 
+        : base("Breathing Activity", 
+              "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
+    {
+    }
+    
+    public override void Run(){
+        DisplayStartingMessage();
+        
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+        
+        while (DateTime.Now < endTime){
+            Console.Write("\nBreathe in...");
+            ShowCountDown(4);
+            
+            Console.Write("\nBreathe out...");
+            ShowCountDown(6);
+            
+            Console.WriteLine();
+        }
+        
+        DisplayEndingMessage();
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
+class Program{
+    static void Main(string[] args){
+        bool quit = false;
+        
+        while (!quit){
+            // Display menu
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            
+            string choice = Console.ReadLine();
+            
+            switch (choice){
+                case "1":
+                    Activity breathingActivity = new BreathingActivity();
+                    breathingActivity.Run();
+                    break;
+                case "2":
+                    Activity reflectionActivity = new ReflectionActivity();
+                    reflectionActivity.Run();
+                    break;
+                case "3":
+                    Activity listingActivity = new ListingActivity();
+                    listingActivity.Run();
+                    break;
+                case "4":
+                    quit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    Thread.Sleep(2000);
+                    break;
+            }
+        }
     }
 }
